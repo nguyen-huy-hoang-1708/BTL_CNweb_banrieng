0294a9f66420f6d289ee157bfd36353c7b84ed0e
ToJupiter <hai.ph225715@sis.hust.edu.vn>
2025-10-20 00:08:35 +0700

    push first change, tommorow add ORM or abstraction layer

---
+++ scripts/backend_setup.sh
@@ -0,0 +1,9 @@
+# cd backend
+# pnpm init
+
+pnpm install express cors helmet morgan cookie-parser bcryptjs jsonwebtoken dotenv winston
+pnpm install prisma @prisma/client
+pnpm install zod swagger-jsdoc swagger-ui-express multer
+
+# Install dev dependencies
+pnpm install -D typescript @types/node @types/express @types/cors @types/jsonwebtoken @types/cookie-parser @types/morgan @types/swagger-jsdoc @types/swagger-ui-express @types/multer nodemon ts-node jest @types/jest supertest @types/supertest
